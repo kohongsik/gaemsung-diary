9129a493c459ca8e47b1df9cea6462884c5b055f
kohongsik <sghs33@naver.com>
2020-04-25 17:07:57 +0900

    commit test

---
+++ src/main/java/com/project/toy/GaemsungDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaemsungDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaemsungDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/toy/GaemsungDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaemsungDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
